9b803470f90bcc2bdf30476f3eafe60332d350dd
Dan B <dbraunby@hotmail.com>
2021-06-01 11:30:45 -0600

    About test

---
+++ src/components/About/__tests__/index.test.js
@@ -0,0 +1,17 @@
+import react from 'react';
+import { render, cleanup } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+import About from '..';
+
+afterEach(cleanup);
+
+describe('About components', () =>{
+    it('renders', () => {
+        render(<About/>);
+    })
+
+    it('matches snapshot DOM node structure', () => {
+        const { asFragment } = render(<About/>);
+        expect(asFragment()).toMatchSnapshot();
+    })
+});
