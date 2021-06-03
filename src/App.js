1a8482ce1e94173ef2a33a37c6425e9fb0bbd2fe
Dan B <dbraunby@hotmail.com>
2021-06-03 11:44:22 -0600

    Contact form setup

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import About from './components/About';
 import Nav from './components/Nav';
 import Gallery from './components/Gallery';
+import ContactForm from './components/Contact';
 
 function App() {
   const [categories] = useState([
@@ -33,6 +34,7 @@ function App() {
         currentCategory={currentCategory}
       ></Nav>
       <main>
+        <ContactForm/>
         <Gallery
           currentCategory={currentCategory}
         ></Gallery>
